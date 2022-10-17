779440dcd621d321b6fdc84a5abce78b1efac057
Fikayo Jetawo <jetawof@gmail.com>
2022-10-17 20:20:34 +0100

    Sidemenu working perfectly

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import React from "react";
 import "./App.css";
-import "./components/style.css";
+// import "./components/style.css";
 import NavBar from "./components/navBar";
 import AboutMe from "./components/AboutMe";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+
 function App() {
   return (
-    <main>
+    // style={{ marginRight: "200px" }}
+    <div>
       <NavBar />
       <AboutMe />
       <Projects />
       <Skills />
       <Contact />
-    </main>
+    </div>
   );
 }
 
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 
-export default function navBar() {
+function NavBar() {
+  // Class Component Code
+  //constructor(props) {
+  //   super(props);
+  //   const sideMenu = <FontAwesomeIcon icon={faBars} />;
+  //   this.state = {
+  //     activeColor: "rgb(156 163 175)",
+  //     sideBarWidth: "0px",
+  //     sideBarValue: sideMenu
+  //   };
+  // }
+  // activeTab(e) {
+  //   e.target.activeColor = "white";
+  // }
+  const sideMenu = <FontAwesomeIcon icon={faBars} />;
+  const close = <FontAwesomeIcon icon={faX} />;
+
+  const [sideBarWidth, setWidth] = useState("0px");
+  const [sideBarBtn, setBtn] = useState(sideMenu);
+  function toggleSideBar() {
+    if (sideBarWidth === "0px") {
+      setWidth("300px");
+      setBtn(close);
+    } else {
+      setWidth("0px");
+      setBtn(sideMenu);
+    }
+  }
   return (
     <div>
       <div className="navBarMain">
@@ -9,11 +38,42 @@ export default function navBar() {
           <h3>Olufikayomi Jetawo</h3>
         </div>
         <div className="navBar">
-          <a href="#projects">Projects</a>
-          <a href="#skills">Skills</a>
-          <a href="#contact">Contact</a>
+          <a
+            href="#projects"
+            className="projects"
+            // style={{ color: this.state.activeColor }}
+            // onClick={() => this.activeTab()}
+          >
+            Projects
+          </a>
+          <a
+            href="#skills"
+            className="skills"
+            // style={{ color: this.state.activeColor }}
+            // onClick={() => this.activeTab()}
+          >
+            Skills
+          </a>
+          <a
+            href="#contact"
+            className="contact"
+            // style={{ color: this.state.activeColor }}
+            // onClick={() => this.activeTab()}
+          >
+            Contact
+          </a>
         </div>
+      </div>
+      <div className="menu" style={{ width: sideBarWidth }}>
+        <button className="sideMenuBtn" onClick={() => toggleSideBar()}>
+          {sideBarBtn}
+        </button>
+        <a href="#about">Home</a>
+        <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
+        <a href="#contact">Contact</a>
       </div>
     </div>
   );
 }
+export default NavBar;
