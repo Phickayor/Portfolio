9faeb4a959cf120aca53b7823189538b1ca13b85
Fikayo Jetawo <jetawof@gmail.com>
2023-08-13 23:35:42 +0100

    projects component

--- frontend/components/Projects.js
+++ frontend/components/Projects.js
@@ -3,17 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode, faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Oswald } from "next/font/google";
-import projects from "../utils/projects"
-const oswald = Oswald({ weight: "700", subsets: ['latin'] })
+import projects from "../utils/projects";
+const oswald = Oswald({ weight: "700", subsets: ["latin"] });
 export default function Projects() {
   const code = <FontAwesomeIcon icon={faCode} />;
   const github = <FontAwesomeIcon icon={faGithub} />;
-  const web = <FontAwesomeIcon icon={faGlobe} />
+  const web = <FontAwesomeIcon icon={faGlobe} />;
   function AddDetails(e) {
     // var targetDiv = e.target.previousElementSibling
     // targetDiv.classList.add("w-full")
     // console.log(targetDiv)
-
   }
   function RemoveDetails(e) {
     // var targetDiv = e.target.previousElementSibling
@@ -22,31 +21,45 @@ export default function Projects() {
   }
   return (
     <div id="works">
-      <h1 data-aos="fade-left" data-aos-duration="1500" className={`opacity-50 text-6xl text-slate-500 ${oswald.className}`}>{code} WORKS</h1>
-      <div className="grid md:grid-cols-2 xl:flex xl:-gap-4 xl:gap-0 gap-10 md:gap-20 mx-5 xl:mx-0 py-20">
+      <h1
+        data-aos="fade-left"
+        data-aos-duration="1500"
+        className={`opacity-50 text-6xl text-slate-500 ${oswald.className}`}
+      >
+        {code} WORKS
+      </h1>
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 my-10 gap-5">
         {projects.map((project, index) => (
-          <a href={project.livelink}
-            onMouseOver={AddDetails}
-            onMouseLeave={RemoveDetails}
-            className=" xl:h-72 h-56 md:h-auto xl:w-56 hover:w-full hover:z-20 hover:grayscale-0 duration-[1.5s] grayscale "
-            key={index}>
-            <div className='md:block hidden absolute text-white bg-slate-500 bg-opacity-40 z-30 opacity-0 hover:opacity-100 xl:py-10 py-5 space-y-2 xl:space-y-5 h-fit w-auto scale-75 lg:scale-100 px-5 overflow-hidden'>
-              <div className="space-y-2">
-                <h1 className="xl:text-xl text-lg font-semibold">{project.name}</h1>
-                <p className="text-purple-900">{project.language}</p>
-              </div>
-              <p className="text-sm">{project.description}</p>
-              <div className="my-5 flex justify-between">
-                <a href={project.githublink}>
-                  <button className='font-semibold hover:bg-purple-700 duration-300 self-center bg-[#121212] hover:scale-105 px-5 py-3 text-sm rounded-lg text-white'>{github} Source Code</button>
-                </a><a href={project.livelink}>
-                  <button className='font-semibold hover:bg-purple-700 duration-300 self-center bg-[#121212] hover:scale-105 px-5 py-3 text-sm rounded-lg text-white'>{web} Live Site</button>
+          <div
+            data-aos="fade-up"
+            data-aos-duration="1500"
+            key={index}
+            className="bg-slate-900 shadow-white shadow-inner text-white rounded-3xl p-5 h-fit self-center"
+          >
+            <img
+              src={project.image}
+              alt={project.name}
+              className="h-36 rounded-2xl w-full object-cover"
+            />
+            <div className="space-y-4 my-5">
+              <h1 className="text-xl font-semibold">{project.name}</h1>
+              <p>{project.description}</p>
+              <div className="flex space-x-5">
+                <a
+                  href={project.githublink}
+                  className="bg-white text-black p-2 rounded-xl font-semibold"
+                >
+                  <button className="self-center">{github} Source Code</button>
+                </a>
+                <a
+                  href={project.livelink}
+                  className="bg-white text-black p-2 rounded-xl font-semibold"
+                >
+                  <button className="self-center">{web} Live Site</button>
                 </a>
               </div>
-
             </div>
-            <img src={project.image} className="w-full hover:mx-0 z-20  relative cursor-pointer h-full object-cover rounded-lg xl:scale-110" />
-          </a>
+          </div>
         ))}
       </div>
     </div>
