444d7ce1271140a4193d232395189a840f47bb5b
Olufikayomi Jetawo <71492979+Phickayor@users.noreply.github.com>
2023-11-18 17:25:15 -0800

    Update NavBar.jsx

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function NavBar() {
   return (
     <div className="sticky top-0 backdrop-blur-md z-20 flex justify-between lg:text-xl py-3 md:py-5 my-4 md:my-8 px-5 border-b-2">
-      <h1 className="self-center">Fikayo</h1>
+      <h1 className="self-center">Olufikayomi</h1>
       <a href="tel:+2347031041271" className="bg-blue text-black p-2 px-6 rounded-lg">
-       Call Me !
+       Call Me🤙🏼
       </a>
     </div>
   );
